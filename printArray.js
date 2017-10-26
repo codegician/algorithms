27270ae8668117a1a30801a091523ea232767b00
ewart <ewartjackson@icloud.com>
2017-10-26 11:11:06 -0400

    Given an array X say [1,3,5,7,9,13], write a program that would iterate through each member of the array and print each value on the screen.

---
+++ printArray.js
@@ -0,0 +1,14 @@
+//Given an array X say [1,3,5,7,9,13], write a program that would iterate through each member of the array and print each value on the screen.
+
+var x = [1,3,5,7,9,13];
+
+function printArray(){
+	for(i=0;i <= x.length; i++){
+		console.log(x[i]);
+	}
+}
+
+printArray(x);
+
+var x = [7,0,8,9,7,8];
+
